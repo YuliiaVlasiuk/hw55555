e3a3fb0ad0b43637892ef315803562c05fa63bfb
YuliiaVlasiuk <vlasyukyulia@gmail.com>
2023-03-17 20:33:47 +0200

    1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,60 @@
+import { Route, Routes } from "react-router-dom";
+import { About } from "../pages/About";
+import { Home } from "../pages/Home";
+import { ProductDetails } from "../pages/ProductDetails";
+import { Products } from "../pages/Products";
+// import { Container, Header, Logo, Link } from "./App.styled";
+
+import { Mission } from "./Mission";
+import { Team } from "./Team";
+import { SharedLayout } from "./SharedLayout";
+import { Reviews } from "./Reviews";
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Routes>
+      <Route path="/" element={<SharedLayout />}>
+        <Route index element={<Home />} />
+        <Route path="about" element={<About />}>
+          <Route path="mission" element={<Mission />} />
+          <Route path="team" element={<Team />} />
+          <Route path="reviews" element={<Reviews />} />
+        </Route>
+        <Route path="products" element={<Products />} />
+        <Route path="products/:id" element={<ProductDetails />} />
+      </Route>
+    </Routes>
   );
 };
+
+
+
+
+
+// export const App = () => {
+//   return (
+//     <Container>
+//       <Header>
+//         <Logo>
+//           <span role="img" aria-label="computer icon">
+//             💻
+//           </span>{" "}
+//           GoMerch Store
+//         </Logo>
+//         <nav>
+//           <Link to="/" end>
+//             Home
+//           </Link>
+//           <Link to="/about">About</Link>
+//           <Link to="/products">Products</Link>
+//         </nav>
+//       </Header>
+//       <Routes>
+//         <Route path="/" element={<Home />} />
+//         <Route path="/about" element={<About />} />
+//         <Route path="/products" element={<Products />} />
+//         <Route path="/products/:id" element={<ProductDetails />} />
+//       </Routes>
+//     </Container>
+//   );
+// };
